1540613e13c133af40695d0db0030cac7e60bcfc
imranhossen21 <imranhossen0586@gmail.com>
2024-12-15 13:02:58 +0600

    done

--- client/src/pages/UpdateJob.jsx
+++ client/src/pages/UpdateJob.jsx
@@ -1,9 +1,73 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AuthContext } from '../providers/AuthProvider'
+import toast from 'react-hot-toast'
 
 const UpdateJob = () => {
   const [startDate, setStartDate] = useState(new Date())
+  const { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const [job, setJob] = useState({})
+  // console.log(job);
+
+
+  useEffect(() => {
+    allJobsData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  const allJobsData = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API}/job/${id}`)
+    setJob(data)
+    setStartDate(new Date(data.deadline))
+
+  }
+
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const title = form.job_title.value;
+    const email = form.email.value;
+    const deadline = startDate;
+    const category = form.category.value;
+    const min_price = form.min_price.value;
+    const max_price = form.max_price.value;
+    const description = form.description.value;
+
+
+    const formData = {
+      bayer: {
+        email,
+        name: user?.displayName,
+        photo: user?.photoURL,
+      },
+      title,
+      deadline,
+      category,
+      max_price,
+      min_price,
+      description,
+      bit_count: job.bit_count
+    }
+    try {
+      await axios.put(`${import.meta.env.VITE_API}/update-job/${id}`, formData)
+      
+      toast.success('Successfully Updated job!')
+      navigate('/my-posted-jobs')
+    }
+    catch (err) {
+      console.error("Error in JobCard:", err.message);
+    }
+
+
+  }
+
+  // const { title, description, category, max_price, min_price } = job || {}
 
   return (
     <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
@@ -12,7 +76,7 @@ const UpdateJob = () => {
           Update a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='job_title'>
@@ -22,6 +86,7 @@ const UpdateJob = () => {
                 id='job_title'
                 name='job_title'
                 type='text'
+                defaultValue={job.title}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
@@ -34,6 +99,7 @@ const UpdateJob = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
@@ -48,20 +114,23 @@ const UpdateJob = () => {
               />
             </div>
 
-            <div className='flex flex-col gap-2 '>
-              <label className='text-gray-700 ' htmlFor='category'>
-                Category
-              </label>
-              <select
-                name='category'
-                id='category'
-                className='border p-2 rounded-md'
-              >
-                <option value='Web Development'>Web Development</option>
-                <option value='Graphics Design'>Graphics Design</option>
-                <option value='Digital Marketing'>Digital Marketing</option>
-              </select>
-            </div>
+            {
+              job.category && <div className='flex flex-col gap-2 '>
+                <label className='text-gray-700 ' htmlFor='category'>
+                  Category
+                </label>
+                <select
+                  name='category'
+                  id='category'
+                  className='border p-2 rounded-md'
+                  defaultValue={job.category}
+                >
+                  <option value='Web Development'>Web Development</option>
+                  <option value='Graphics Design'>Graphics Design</option>
+                  <option value='Digital Marketing'>Digital Marketing</option>
+                </select>
+              </div>
+            }
             <div>
               <label className='text-gray-700 ' htmlFor='min_price'>
                 Minimum Price
@@ -70,6 +139,7 @@ const UpdateJob = () => {
                 id='min_price'
                 name='min_price'
                 type='number'
+                defaultValue={job.min_price}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
@@ -82,6 +152,7 @@ const UpdateJob = () => {
                 id='max_price'
                 name='max_price'
                 type='number'
+                defaultValue={job.max_price}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
@@ -94,6 +165,7 @@ const UpdateJob = () => {
               className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               name='description'
               id='description'
+              defaultValue={job.description}
               cols='30'
             ></textarea>
           </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 
 const port = process.env.PORT || 9000
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eedxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +23,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    // await client.db('admin').command({ ping: 1 })
+    const db = client.db("soloSphere")
+    const jobsCollections = db.collection('jobs')
+
+    app.post('/add-job', async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollections.insertOne(newJob);
+      res.send(result)
+
+    })
+
+    app.get('/jobs', async (req, res) => {
+      const result = await jobsCollections.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/jobs/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { 'bayer.email': email }
+      const result = await jobsCollections.find(query).toArray()
+      res.send(result)
+    })
+    app.delete('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollections.deleteOne(query);
+      res.send(result)
+    })
+    app.get('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollections.findOne(query);
+      res.send(result)
+    })
+
+    app.put('/update-job/:id', async (req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = { _id: new ObjectId(id) }
+      const updatedJob = {
+        $set: jobData
+        
+      }
+      const options = { upsert: true }
+      const result = await jobsCollections.updateOne(query, updatedJob, options);
+      res.send(result)
+    })
+
+
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
